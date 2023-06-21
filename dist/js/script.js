fb0c5beeb824777809e0a4427fecf9595e4e71ae
Olha Ovcharenko <ovcharenko.olga.2014@gmail.com>
2023-06-21 23:13:50 +0300

    add function initAccordion

--- dist/js/script.js
+++ dist/js/script.js
@@ -60,9 +60,11 @@ const select = {
       thisProduct.data =data;
 
       thisProduct.renderInMenu();
+      thisProduct.initAcordion();
 
       console.log('New Product:', thisProduct);
-    };
+
+    }
     renderInMenu(){
       const thisProduct = this;
 
@@ -79,9 +81,30 @@ const select = {
 
       /* add element to menu */
       menuContainer.appendChild(thisProduct.element);
-    };
+    }
 
-  };
+    initAcordion(){
+      const thisProduct = this;
+
+       /* find the clickable trigger (the element that should react to clicking) */
+    
+      /* START: add event listener to clickable trigger on event click */
+      clickableTrigger.addEventListener('click', function(event) {
+       /* prevent default action for event */
+
+        /* find active product (product that has active class) */
+
+        /* if there is active product and it's not thisProduct.element, remove class active from it */
+
+        /* toggle active class on thisProduct.element */
+    });
+
+  }
+}
+
+    }
+
+  }
 
   
   const app = {
