e819947a74d7832ba06a5d5b0d8db695b9b5ffb1
Olha Ovcharenko <ovcharenko.olga.2014@gmail.com>
2023-07-09 13:43:22 +0300

    update files

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,69 @@
 
-import {settings, select,} from './settings.js';
+import {settings, select, classNames} from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
 
 
 const app = {
+
+  initPages: function() {
+    const thisApp = this;
+
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
+
+    const idFromHash = window.location.hash.replace('#/', '');
+    
+    
+    let pageMatchingHash = thisApp.pages[0].id;
+    
+    for(let page of thisApp.pages){
+      if(page.id == idFromHash){
+        pageMatchingHash = page.id;
+        break;
+        
+      }
+    }
+    //console.log('pageMatchingHash2', pageMatchingHash);
+    thisApp.activatePage(pageMatchingHash);
+    
+    for(let link of thisApp.navLinks){
+      link.addEventListener('click', (event)=>{
+
+        const clickedElement = event.currentTarget;
+        event.preventDefault();
+       
+
+        // get page ID from href attr.
+        const id = clickedElement.getAttribute('href').replace('#', '');
+        //console.log(clickedElement,'clickedElement');
+        // run thisApp.activatePage() with ID
+        thisApp.activatePage(id);
+        //console.log(id, 'id');
+        // change URL hash, add / to prevent scrolling to #
+
+        window.location.hash = '#/' + id;
+
+      });
+    }
+
+  },
+
+  activatePage: function(pageId){
+
+    const thisApp = this;
+
+    for(let page of thisApp.pages) {
+      page.classList.toggle(classNames.pages.active, page.id == pageId);
+    }
+    for(let link of thisApp.navLinks) {
+      link.classList.toggle(
+        classNames.nav.active,
+        link.getAttribute('href') == '#' + pageId
+      );
+    }
+  },
+
   initMenu: function () {
     const thisApp = this;
     // console.log('thisApp.data:', thisApp.data);
@@ -59,6 +118,7 @@ const app = {
     thisApp.initData();
     thisApp.initMenu();
     thisApp.initCart();
+    thisApp.initPages();
   },
 
 };
