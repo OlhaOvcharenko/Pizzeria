28a8daaf1388b479b2df41f6d03a852d6764e877
Olha Ovcharenko <ovcharenko.olga.2014@gmail.com>
2023-07-12 22:02:10 +0300

    change code for class 'Bopking'

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,5 +1,7 @@
 import {select,templates} from '../settings.js';
 import AmountWidget  from './AmountWidget.js';
+import DatePicker from './DatePicker.js';
+import HourPicker from './HourPicker.js';
 
 class Booking {
   constructor(element) {
@@ -17,12 +19,16 @@ class Booking {
 
     this.dom.peopleAmount = document.querySelector(select.booking.peopleAmount);
     this.dom.hoursAmount = document.querySelector(select.booking.hoursAmount);
+    this.dom.datePicker = document.querySelector(select.widgets.datePicker.wrapper);
+    this.dom.hourPicker = document.querySelector(select.widgets.hourPicker.wrapper);
   }
 
   initWidgets(){
 
     this.dom.peopleAmountWidget = new AmountWidget(this.dom.peopleAmount);
     this.dom.hoursAmountWidget = new AmountWidget(this.dom.hoursAmount);
+    this.dom.datePickerWidget = new DatePicker(this.dom.datePicker);
+    this.dom.hourPickerWidget = new HourPicker(this.dom.hourPicker);
 
     this.dom.peopleAmount.addEventListener('updated', function () {
       // Handle people amount update
@@ -30,6 +36,12 @@ class Booking {
 
     this.dom.hoursAmount.addEventListener('updated', function () {
       // Handle hours amount update
+    });
+
+    this.dom.datePicker.addEventListener('updated', function () {
+    });
+
+    this.dom.hourPicker.addEventListener('updated', function (){
     });
 
   }
