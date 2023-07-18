87f505c767b2f01029e51b4fb4f1056ba63ccd79
Olha Ovcharenko <ovcharenko.olga.2014@gmail.com>
2023-07-18 18:26:00 +0300

    fix code

--- src/js/components/Home.js
+++ src/js/components/Home.js
@@ -19,13 +19,59 @@ class Home {
     element.innerHTML = generatedHTML;
     thisHome.pages = document.querySelector(select.containerOf.pages).children;
     thisHome.homeLinks = document.querySelectorAll(select.nav.homeLinks);
-    hisApp.navLinks = document.querySelectorAll(select.nav.links);
-  
+    thisHome.navLinks = document.querySelectorAll(select.nav.links);
+    
+    const idFromHash = window.location.hash.replace('#/', '');
+    
+    let pageMatchingHash = thisHome.pages[0].id;
+
+    for(let page of thisHome.pages){
+      if(page.id == idFromHash){
+        pageMatchingHash = page.id;
+        break;
+        
+      }
+    }
+    
+    thisHome.activatePage(pageMatchingHash);
+
+    for(let link of thisHome.homeLinks){
+      link.addEventListener('click', (event)=>{
+
+        const clickedElement = event.currentTarget;
+        event.preventDefault();
+       
+
+        // get page ID from href attr.
+        const id = clickedElement.getAttribute('href').replace('#', '');
+        //console.log(clickedElement,'clickedElement');
+        // run thisApp.activatePage() with ID
+        thisHome.activatePage(id);
+        //console.log(id, 'id');
+        // change URL hash, add / to prevent scrolling to #
+
+        window.location.hash = '#/' + id;
+      });
+    }
   }
 
+  activatePage(pageId){
+
+    const thisHome = this;
+
+    for(let page of thisHome.pages) {
+      page.classList.add(classNames.pages.active, page.id == pageId);
+    }
+    for(let link of thisHome.navLinks) {
+      link.classList.toggle(
+        classNames.nav.active,
+        link.getAttribute('href') == '#' + pageId
+      );
+    }
+  }
 
   initSlider(){
-   new Flickity(select.all.carousel, {
+    new Flickity(select.all.carousel, {
       prevNextButtons: false,
       autoPlay: true,
       imagesLoaded: true,
